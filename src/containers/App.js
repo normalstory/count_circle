47b7721e2e17bb33c05ddf0f68129a0b4e8ea109
thinknormal <nornalstory@gmail.com>
2020-10-22 18:47:25 +0900

    1. 기본 프리젠테이셔널 컴포넌트 생성

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
+import Counter from '../components/Counter'
 
 export default class App extends Component{
     render(){
         return(
-            <div>Hello React</div>
+            <div>
+                <Counter></Counter>
+            </div>
         )
     }
 }
