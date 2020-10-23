a757cbf0c30159cb7d8f905f3c81fee9d46cbc8f
thinknormal <nornalstory@gmail.com>
2020-10-23 10:03:10 +0900

    4. 리덕스 devToolsExtension 세팅

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import { createStore } from 'redux';    // 1) 리덕스 불러오기
 import reducers from './reducers';      // 1) 작성한 리듀서 불러오기
 import { Provider } from 'react-redux'; // 3) Provider를 불러와서, 리덕스 스토어를 리액트 컴포넌트에 속성props으로 연동 
 
-const store = createStore(reducers);    // 2) 리덕스 스토어 생성, 리듀서 연동  
-
+// const store = createStore(reducers);    // 2) 리덕스 스토어 생성, 리듀서 연동  
+const store = createStore(reducers, window.devToolsExtension && window.devToolsExtension());    // 리덕스 개발자 툴 연동
 
 ReactDOM.render(
   <Provider store={store}>
